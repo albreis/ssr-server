a53c172a3b5ba67f3757c8488f11dead457cf152
Albreis - Design & Programação <albreisdev@gmail.com>
2020-08-05 22:28:45 -0300

    first commit

---
+++ sample/index.mjs
@@ -0,0 +1,25 @@
+import express from 'express'
+import puppeteer from 'puppeteer'
+import redis from 'redis'
+const app = express();
+app.get('[^\.]+?', loadUrl);
+const RENDER_CACHE = redis.createClient(); 
+async function loadUrl(req, res) {
+  const cache_url = 'https://{app}' + req.path;
+  const url = 'https://{app}' + req.originalUrl;
+  var html;
+  RENDER_CACHE.get(cache_url, async (err, data) => { 
+    if(data && !req.query.updatecache) {
+      res.send(data)
+    } else {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(url, {waitUntil: "networkidle2"});
+      await page.waitFor(2000);
+      html = await page.evaluate(() => {return document.documentElement.innerHTML;});
+      RENDER_CACHE.set(cache_url, html)
+      res.send(html);
+    }
+  })  
+}
+app.listen({port}, () => console.log('Server started. Press Ctrl+C to quit'));
