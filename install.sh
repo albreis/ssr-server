f03d451711e34c11892c9771bcdfc15b91ede650
Albreis - Design & Programação <albreisdev@gmail.com>
2020-08-07 22:23:13 -0300

    Resstruturação

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ sudo apt install nginx nodejs redis-server python-certbot-nginx php php-fpm comp
 sudo npm install -g pm2;
 composer require phpseclib/phpseclib;
 composer require vlucas/phpdotenv;
+read -p  "Digite o domínio do seu servidor SSR:" domain;
+sudo rm -f /etc/nginx/sites-enabled/$domain.conf;
+sudo cp -f sample/server.conf /etc/nginx/sites-enabled/$domain.conf;
+sudo sed -i "s/{domain}/$domain/g" /etc/nginx/sites-enabled/$domain.conf;
+sudo nginx -s reload;
+sudo certbot --nginx --noninteractive --agree-tos -d $domain -m 'contato@albreis.com.br';
